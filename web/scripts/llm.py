d58d7ae66276a43c47602634c2e95121c02b616b
David Nguyen <davido.nguyen1212@gmail.com>
2024-05-30 11:13:54 -0700

    refactor llm summary script for split json

--- web/scripts/llm.py
+++ web/scripts/llm.py
@@ -3,53 +3,40 @@ import json
 import os
 
 url_to_summary = {}
+id_to_url = {}
 
-def summarize():
-     id_count = 0
+def summarize(path: str):
+    with open(path, "r") as map:
+        id_to_url = json.load(map)   
 
-     dev_path = os.path.abspath("../../DEV")
+        for id, url in id_to_url.items():
+            llm_url = "http://localhost:11434/api/generate"
+            prompt = "Here is the content of a webpage in 6-10 words analyze the it and summarize the page.\
+                            Make sure the summary is short and speedy, here is the url: " + str(url)
+            llm_data = {
+                "model": "phi3",
+                "prompt": prompt,
+                "stream": False
+            }
 
-     for root, dirs, files in os.walk(dev_path): #loop through DEV directory and subdirectories
-        dirs.sort()                 
-        for file in sorted(files):  
-            file_path = os.path.join(root, file) #Get absolute path to file so we can open it
-                
-            with open(file_path, "r") as f: #open file then grab data from json file
-                data = json.load(f)
-                
-                url = data.get("url", "") # Our data from the json
-
-                llm_url = "http://localhost:11434/api/generate"
-                prompt = "Here is the content of a webpage in 6-10 words analyze the it and summarize the page.\
-                          Make sure the summary is short and speedy, here is the url: " + str(url)
-                llm_data = {
-                    "model": "phi3",
-                    "prompt": prompt,
-                    "stream": False
+            headers = {
+                'Content-Type': 'application/json'
                 }
 
-                headers = {
-                    'Content-Type': 'application/json'
-                }
+            response = requests.post(llm_url, data=json.dumps(llm_data), headers=headers)
 
-                response = requests.post(llm_url, data=json.dumps(llm_data), headers=headers)
+            if response.status_code == 200:
+                summary = response.json()["response"]
+            else:
+                summary = "Unable to retrieve page details."
 
-                if response.status_code == 200:
-                    summary = response.json()["response"]
-                else:
-                    summary = "Unable to retrieve page details."
-
-                url_to_summary[id_count] = summary
-                print(f"{id_count:<6} {summary}")
-
-                id_count += 1
-
-                
+            url_to_summary[id] = summary
+            print(f"{id:<6} {summary}")
                 
                 
 
 if __name__ == "__main__":
-    summarize()
+    summarize("../../databases/id_to_url.json")
 
     with open("../../databases/id_to_summary.json", "w") as out:
         json.dump(url_to_summary, out, indent=4)
